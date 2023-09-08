eb04b3cacc99b43a55181b6a7e950d49e96e0ab4
Ephmeral <2669128599@qq.com>
2023-09-08 20:47:00 +0800

    Add: signup by phone

--- routes/api.go
+++ routes/api.go
@@ -2,6 +2,7 @@
 package routes
 
 import (
+	"gohub/app/http/controllers/api/v1/auth"
 	"net/http"
 
 	"github.com/gin-gonic/gin"
@@ -20,5 +21,11 @@ func RegisterAPIRoutes(r *gin.Engine) {
 				"Hello": "World!",
 			})
 		})
+		authGroup := v1.Group("/auth")
+		{
+			suc := new(auth.SignupController)
+			// 判断手机是否已注册
+			authGroup.POST("/signup/phone/exist", suc.IsPhoneExist)
+		}
 	}
 }
