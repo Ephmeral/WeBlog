04edab28a3e99c7b4c851e712c40a765caae1b09
Ephmeral <2669128599@qq.com>
2023-09-08 17:31:55 +0800

    Add: config files init

--- main.go
+++ main.go
@@ -1,14 +1,28 @@
 package main
 
 import (
+	"flag"
 	"fmt"
 
 	"gohub/bootstrap"
+	btsConfig "gohub/config"
+	"gohub/pkg/config"
 
 	"github.com/gin-gonic/gin"
 )
 
+func init() {
+	// 加载 config 目录下的配置信息
+	btsConfig.Initialize()
+}
+
 func main() {
+	// 配置初始化，依赖命令行 --env 参数
+	var env string
+	flag.StringVar(&env, "env", "", "加载 .env 文件，如 --env=testing 加载的是 .env.testing 文件")
+	flag.Parse()
+	config.InitConfig(env)
+
 	// 创建一个Gin实例
 	router := gin.New()
 
